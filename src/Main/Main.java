59b1af73c3aef650e947c89bb01497565b606203
Jorge <jlsanchezj2000@gmail.com>
2023-07-02 23:32:25 -0600

    Nuevo cambio

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,5 +5,6 @@ package Main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Java!");
+        System.out.println("la coneccion con github se ha hecho");
         }
     }
