21a3f91be33edce392c5c00a44938fe1eeb502fb
Jorge <jlsanchezj2000@gmail.com>
2023-07-02 23:46:50 -0600

    otro cambio

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Java!");
         System.out.println("la coneccion con github se ha hecho");
+        System.out.println("otro commit");
         }
     }
